c90e0a27e1b0df58d0da56b1608bacf35e7b092f
to4to <justjustifying@outlook.com>
2024-05-19 18:28:57 +0530

    chore: Add DecentralizedStableCoin contract with collateral support

--- src/DecntralizedStableCoin.sol
+++ src/DecntralizedStableCoin.sol
@@ -2,6 +2,13 @@
 pragma solidity ^0.8.21;
 
 
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Himanshu Kumar Singh
+ * Collateral : Exogeous (ETH & BTC)
+ * 
+ */
 contract DecentralizedStableCoin {
     constructor() {
         
