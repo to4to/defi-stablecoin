783764873f2d9ebe9fb6cee3d131bcc6f2f896e0
to4to <justjustifying@outlook.com>
2024-05-19 18:36:23 +0530

    chore: Add OpenZeppelin Contracts submodule

--- src/DecntralizedStableCoin.sol
+++ src/DecntralizedStableCoin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.21;
 
-
+import{ERC20Burable , ERC20} from "@openzepplin/contracts/token/ERC20/extensions/ERC20Burnable.sol"
 
 /**
  * @title DecentralizedStableCoin
