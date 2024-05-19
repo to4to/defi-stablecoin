635fbc2b1c298438786716c68f6716a2099eb72c
to4to <justjustifying@outlook.com>
2024-05-19 22:36:01 +0530

    feat: Add ERC20Burnable extension to DecentralizedStableCoin contract

--- src/DecntralizedStableCoin.sol
+++ src/DecntralizedStableCoin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.21;
 
-import{ERC20Burable , ERC20} from "@openzepplin/contracts/token/ERC20/extensions/ERC20Burnable.sol"
+import{ERC20Burable , ERC20} from "@openzepplin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /**
  * @title DecentralizedStableCoin
@@ -13,8 +13,8 @@ import{ERC20Burable , ERC20} from "@openzepplin/contracts/token/ERC20/extensions
  * This is Contract ment to be governed by DSCEngine. This is ERC-20 implementation of our Stable Coin System.
  * 
  */
-contract DecentralizedStableCoin {
-    constructor() {
+contract DecentralizedStableCoin  is ERC20Burnable{
+    constructor()ERC20("DecentralizedStableCoin","DSC") {
         
     }
 }
