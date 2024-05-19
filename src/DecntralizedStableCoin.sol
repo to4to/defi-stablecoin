3c5075dfee1ccf11048da2d958d3758ae44540e6
to4to <justjustifying@outlook.com>
2024-05-19 18:30:49 +0530

    feat: Add DecentralizedStableCoin contract with collateral and algorithmic minting

--- src/DecntralizedStableCoin.sol
+++ src/DecntralizedStableCoin.sol
@@ -7,6 +7,10 @@ pragma solidity ^0.8.21;
  * @title DecentralizedStableCoin
  * @author Himanshu Kumar Singh
  * Collateral : Exogeous (ETH & BTC)
+ * Minting : Algorithmic
+ * Relative Stability : Pegged To USD
+ * 
+ * This is Contract ment to be governed by DSCEngine. This is ERC-20 implementation of our Stable Coin System.
  * 
  */
 contract DecentralizedStableCoin {
