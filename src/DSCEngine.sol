17341fccfab2a3e3448136fae03c7946c8240eec
to4to <justjustifying@outlook.com>
2024-05-23 22:37:35 +0530

    feat: Update remappings in foundry.toml and add OracleLib import to DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -34,7 +34,7 @@ error DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
 ///// Types
 ////////////////////////////
 
-
+ using OracleLib for AggregatorV3Interface;
 
 
 
@@ -44,29 +44,79 @@ error DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
 //////// State Variables
 ///////////////////////////////
 
+DecentralizedStableCoin private immutable i_dsc;
 
+    uint256 private constant LIQUIDATION_THRESHOLD = 50; //  200% over-collateralized
+    uint256 private constant LIQUIDATION_BONUS = 10; //  10% discount when liquidating
+    uint256 private constant LIQUIDATION_PRECISION = 100;
+    uint256 private constant MIN_HEALTH_FACTOR = 1e18;
+    uint256 private constant PRECISION = 1e18;
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant FEED_PRECISION = 1e8;
+
+
+
+
+     /// @dev Mapping of token address to price feed address
+    mapping(address collateralToken => address priceFeed) private s_priceFeeds;
+    /// @dev Amount of collateral deposited by user
+    mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposited;
+    /// @dev Amount of DSC minted by user
+    mapping(address user => uint256 amount) private s_DSCMinted;
+    /// @dev If we know exactly how many tokens we have, we could make this immutable!
+    address[] private s_collateralTokens;
 
 //////////////////////////////
 /////Events
 /////////////////////////////
-
+event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+    event CollateralRedeemed(address indexed redeemFrom, address indexed redeemTo, address token, uint256 amount);
 
 /////////////////////////////
 //////Modifier
 /////////////////////////////
 
+ modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__TokenNotAllowed(token);
+        }
+        _;
+    }
 
 
+constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+        }
+        // These feeds will be the USD pairs
+        // For example ETH / USD or MKR / USD
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            s_collateralTokens.push(tokenAddresses[i]);
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
 
-constructor(address[] tokenAddresses,address[] priceFeedAddresses, address dscEngine){
-
-
-
-
-    
-}
-
-
+//////////////////////////////////////
+/////////External
+////////////////////////////////////
+function depositCollateralAndMintDsc(
+        address tokenCollateralAddress,
+        uint256 amountCollateral,
+        uint256 amountDscToMint
+    )
+        external
+    {
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintDsc(amountDscToMint);
+    }
 
 
 }
