f914200453f3e09cfca806106257f0422e713fb1
to4to <justjustifying@outlook.com>
2024-05-23 21:24:38 +0530

    feat: Add OracleLib and ReentrancyGuard imports to DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,11 +1,52 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.21;
 
+import { OracleLib, AggregatorV3Interface } from "./libraries/OracleLib.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
+
+/**
+ * @author github.com/to4to
+ * @title DSCEngine
+ * 
+ * 
+ */
 
 
-contract DSCEngine {
+contract DSCEngine  is ReentrancyGuard{
+
+////////////////////////////////
+//////// Errors
+//////////////////////////////
+
+
+
+/////////////////////////////
+///// Types
+////////////////////////////
+
+
+
+
+
+
+
+////////////////////////////////
+//////// State Variables
+///////////////////////////////
+
+
+
+constructor(address[] tokenAddresses,address[] priceFeedAddresses, address dscEngine){
+
 
 
 
     
+}
+
+
+
+
 }
