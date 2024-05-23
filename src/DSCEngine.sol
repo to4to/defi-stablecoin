7fac709f3be01974279bb7d461ebe356685960dc
to4to <justjustifying@outlook.com>
2024-05-23 23:04:43 +0530

    feat: Update import statement for DecentralizedStableCoin in DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.21;
 
 import { OracleLib, AggregatorV3Interface } from "./libraries/OracleLib.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {DecentralizedStableCoin} from "./DecntralizedStableCoin.sol";
 
 /**
  * @author github.com/to4to
